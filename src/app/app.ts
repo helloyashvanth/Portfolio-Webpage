b8b34cefdb50558b658a7a444ee8f86744c269dc
helloyashvanth <hello.yashvanth@gmail.com>
2025-11-03 22:12:23 +0530

    Skill Section completed

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './navbar/navbar';
 import { HeroSection } from './hero-section/hero-section';
+import { SkillSection } from './skill-section/skill-section';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, HeroSection],
+  imports: [RouterOutlet, Navbar, HeroSection, SkillSection],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/skill-section/skill-section.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skill-section',
+  imports: [],
+  templateUrl: './skill-section.html',
+  styleUrl: './skill-section.css',
+})
+export class SkillSection {
+
+}
