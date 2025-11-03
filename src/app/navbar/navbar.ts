166908ff6769ca4eaa00d717cb8a27d77b466485
helloyashvanth <hello.yashvanth@gmail.com>
2025-11-03 22:48:10 +0530

    Dark/Light toggle added

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -8,4 +8,31 @@ import { Component } from '@angular/core';
 })
 export class Navbar {
 
+  isDarkMode = false;
+  currentMode = 'Light';
+
+  constructor() {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'Dark') {
+      this.isDarkMode = true;
+      document.body.classList.add('dark-theme');
+      this.currentMode = 'Dark';
+    } else {
+      this.currentMode = 'Light';
+    }
+  }
+
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+
+    if (this.isDarkMode) {
+      document.body.classList.add('dark-theme');
+      localStorage.setItem('theme', 'Dark');
+      this.currentMode = 'Dark';
+    } else {
+      document.body.classList.remove('dark-theme');
+      localStorage.setItem('theme', 'Light');
+      this.currentMode = 'Light';
+    }
+  }
 }
