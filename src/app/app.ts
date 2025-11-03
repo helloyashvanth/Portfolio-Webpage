ef4b900d9b7a001c631869d5308b3935f718d0f3
helloyashvanth <hello.yashvanth@gmail.com>
2025-11-03 22:33:44 +0530

    Project Section completed

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Navbar } from './navbar/navbar';
 import { HeroSection } from './hero-section/hero-section';
 import { SkillSection } from './skill-section/skill-section';
+import { ProjectSection } from './project-section/project-section';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, HeroSection, SkillSection],
+  imports: [RouterOutlet, Navbar, HeroSection, SkillSection, ProjectSection],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/project-section/project-section.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-section',
+  imports: [],
+  templateUrl: './project-section.html',
+  styleUrl: './project-section.css',
+})
+export class ProjectSection {
+
+}
